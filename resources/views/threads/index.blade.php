7546dfaac0fd70a8b7d7ce81f82265464639e1cc
Rafael von Saltiél <rafaelsaltiel@gmail.com>
2018-04-10 22:51:11 -0300

    Laravel Mix, Estrutura do layout inicial

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div id="app">
+    <example-component>
+        Carregando
+    </example-component>
+</div>
+@endsection
+
+@section('scripts')
+    <script src="/js/threads.js"></script>
+@endsection
